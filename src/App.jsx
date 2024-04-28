bb661c6395ea4598d568b559a4f35490c932c6ea
Samuel Adu <theadusamuel@gmail.com>
2024-04-28 01:16:38 +0100

    remove resume page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Resume from './pages/Resume';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/resume" element={<Resume />} />
       </Routes>
     </BrowserRouter>
   );
