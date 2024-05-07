c29d4af2973f16a882f892f257932b11a432a14d
Samuel Adu <theadusamuel@gmail.com>
2024-05-07 14:32:09 +0100

    use hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Resume from './pages/Resume.jsx';
 import Layout from './components/Layout.jsx';
 import './index.css';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<Layout />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/resume" element={<Resume />} />
-    </Route>
-  )
-);
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />{' '}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
