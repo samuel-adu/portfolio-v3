bea5e90e9cb18e8d8350f18673b7ab2c84f9b27f
Samuel Adu <theadusamuel@gmail.com>
2024-05-07 15:21:50 +0100

    change router setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Resume from './pages/Resume.jsx';
-import Layout from './components/Layout.jsx';
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="resume" element={<Resume />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
