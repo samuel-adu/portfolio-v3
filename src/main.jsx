5dcf52ef7be8adfd6f8817796ba2eebd12f529f2
Samuel Adu <theadusamuel@gmail.com>
2024-05-07 14:09:47 +0100

    setup router using createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Resume from './pages/Resume.jsx';
 import Layout from './components/Layout.jsx';
 import './index.css';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/resume" element={<Resume />} />
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/resume" element={<Resume />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />{' '}
   </React.StrictMode>
 );
