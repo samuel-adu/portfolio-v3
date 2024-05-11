6a14edd2fb56eb156432204e1e32b3ee175015e3
Samuel Adu <theadusamuel@gmail.com>
2024-05-11 12:14:11 +0100

    add routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Resume from './pages/Resume.jsx';
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
+import Layout from './components/Layout.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/resume" element={<Resume />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
