67799a8840b4f0c57cb37336c74db22d4b592caa
Samuel Adu <samueladu.dev@gmail.com>
2025-05-07 08:48:30 +0100

    redesign project card

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,12 @@ import { skills } from '../data';
 function Skills() {
   return (
     <section id="skills" className="skill skills-section">
-      <h2 className="section-heading skills-section-heading">Tech Stack</h2>
+      <h2 className="section-heading skills-section-heading">My Stack</h2>
+      {/* <p>
+        I'm proficient in a wide range of technologies and constantly learning
+        new ones.
+      </p> */}
+
       <ul className="skill__list">
         {skills.map((item) => (
           <li key={item.name} className="skill__item">
